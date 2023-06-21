3209242ed04cec40e25cb002dfd2f8688eb5824c
sheringugi <sheri.ngugi17@gmail.com>
2023-06-21 07:30:00 +0300

    complete assignment

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,5 @@
 class AuthorsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   
   def show
     author = Author.find(params[:id])
@@ -6,13 +7,33 @@ class AuthorsController < ApplicationController
     render json: author
   end
 
-  def create
-    author = Author.create(author_params)
-
+  def create 
+    author= Author.create!(author_params)
     render json: author, status: :created
   end
 
+  # def create 
+  #   author= Author.create(author_params)
+  #   if author.valid?
+  #   render json: author, status: :created
+  #   else
+  #     render json: {errors: author.errors}, status: :unprocessable_entity
+  #   end
+  # end
+
+  # def create
+  #   author = Author.create!(author_params)
+  #   render json: author, status: :created
+  # rescue ActiveRecord::RecordInvalid => invalid
+  #   render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+    
+  # end
+
   private
+
+  def render_unprocessable_entity_response(invalid)
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+  end
   
   def author_params
     params.permit(:email, :name)
